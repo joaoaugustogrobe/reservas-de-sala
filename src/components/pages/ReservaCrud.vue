dc3ff464d52e2ee519480053445b8214b2345aaf
João Augusto Grobe Castilho <joaocastilho@alunos.utfpr.edu.br>
2019-12-02 20:16:21 -0300

    Forms e validação da criação de reserva

--- src/components/pages/ReservaCrud.vue
+++ src/components/pages/ReservaCrud.vue
@@ -1,45 +1,76 @@
 <template>
   <content-app
-    icon="calendar-day"
-    title="Gestão de Ambientes"
+    icon="door-open"
+    title="Gestão de Reservas"
     subtitle="Página para inserir, alterar e remover reservas."
   >
-    <h1>Cadastro de usuários</h1>
+    <h1>Cadastro de Reservas</h1>
     <hr />
     <div class="form">
       <div class="row">
         <div class="col-12 col-md-6">
           <div class="form-group">
-            <label for="codigo">Usuário*</label>
-            <select class="form-control" ref="form-user" v-model="user">
-              <option v-for="user in users" v-bind:key="user.id">{{user.nome}} -- {{user.codigo}}</option>
-              </select>
-            <div class="invalid-feedback">Código invalido.</div>
+            <label for="nome">Usuário*</label>
+            <select class="form-control" ref="form-usuario" v-model="usuario">
+              <option
+                v-for="usuario in usuarios"
+                v-bind:key="usuario.id"
+                v-bind:id="usuario.id"
+              >{{usuario.nome}} -- {{usuario.codigo}}</option>
+            </select>
+            <div class="invalid-feedback">Defina um usuário.</div>
           </div>
         </div>
-        <div class="row">
+
         <div class="col-12 col-md-6">
           <div class="form-group">
-            <label for="ambiente">Ambiente*</label>
+            <label for="nome">Ambiente*</label>
             <select class="form-control" ref="form-ambiente" v-model="ambiente">
-              <option v-for="ambiente in ambientes" v-bind:key="ambiente.id">{{ambiente.nome}} -- {{ambiente.codigo}}</option>
-              </select>
-            <div class="invalid-feedback">Código invalido.</div>
+              <option
+                v-for="ambiente in ambientes"
+                v-bind:key="ambiente.id"
+                v-bind:id="ambiente.id"
+              >{{ambiente.nome}} -- {{ambiente.descricao.substr(0,10)}}</option>
+            </select>
+            <div class="invalid-feedback">Defina um ambiente.</div>
           </div>
         </div>
+
         <div class="col-12 col-md-6">
           <div class="form-group">
-            <label for="datepicker">Data*</label>
-            <datepicker class="datepicker" placeholder="__/__/____"></datepicker>
+            <label for="nome">Data*</label>
+            <datepicker v-model="data" name="uniquename" ref="form-data" id="form-data"></datepicker>
+            <div class="invalid-feedback">Defina uma data para o agendamento.</div>
           </div>
         </div>
+
+        <div class="col-6 col-md-3">
+          <div class="form-group">
+            <label for="nome">Horário de inicio*</label>
+            <timeselector class="form-control" v-model="timeInicio" id="form-timeInicio"></timeselector>
+            <div class="invalid-feedback">Defina um horário.</div>
+          </div>
+        </div>
+
+        <div class="col-6 col-md-3">
+          <div class="form-group">
+            <label for="nome">Horário de término*</label>
+            <timeselector
+              class="form-control"
+              v-model="timeFinal"
+              id="form-timeFinal"
+            ></timeselector>
+            <div class="invalid-feedback">{{timeFinalError}}</div>
+          </div>
+        </div>
+
         <hr />
 
         <div class="row col-12 mb-3">
           <div class="col-12 d-flex justify-content-end">
             <button class="btn btn-secondary mr-2" v-on:click="limpaCampos">Cancelar</button>
-            <button class="btn btn-primary" v-on:click="criarUsuario" v-if="indexEdicao == -1">Cadastrar!</button>
-            <button class="btn btn-primary" v-on:click="salvarUsuario" v-if="indexEdicao != -1">Salvar!</button>
+            <button class="btn btn-primary" v-on:click="criar" v-if="indexEdicao == -1">Cadastrar!</button>
+            <button class="btn btn-primary" v-on:click="salvar" v-if="indexEdicao != -1">Salvar!</button>
           </div>
         </div>
         <hr />
@@ -48,17 +79,21 @@
           <thead>
             <tr>
               <th scope="col" style="width:60%">Nome</th>
-              <th scope="col" style="width:20%">Código</th>
+              <th scope="col" style="width:20%">Lotacao máxima</th>
               <th scope="col" style="width:10%">Ações</th>
             </tr>
           </thead>
           <tbody>
-            <tr v-for="(usuario, index) in list" v-bind:index="index" v-bind:key="usuario.id">
-              <td>{{usuario.nome}}</td>
-              <td >{{usuario.codigo}}</td>
+            <tr v-for="(ambiente, index) in list" v-bind:index="index" v-bind:key="ambiente.id">
+              <td>{{ambiente.nome}}</td>
+              <td>{{ambiente.lotacao}}</td>
               <td class="d-flex justify-content-end">
-                <button class="btn btn-warning mx-3" v-on:click="editar"><i class="fa fa-pen"></i></button>
-                <button class="btn btn-danger" v-on:click="remover"><i class="fas fa-trash"></i></button>
+                <button class="btn btn-warning mx-3" v-on:click="editar">
+                  <i class="fa fa-pen"></i>
+                </button>
+                <button class="btn btn-danger" v-on:click="remover">
+                  <i class="fas fa-trash"></i>
+                </button>
               </td>
             </tr>
           </tbody>
@@ -69,114 +104,181 @@
 </template>
 
 <script>
+import Datepicker from "vuejs-datepicker/dist/vuejs-datepicker.esm.js";
+import Timeselector from "vue-timeselector";
 import Content from "./../template/Content";
 import axios from "axios";
-import Datepicker from "vuejs-datepicker/dist/vuejs-datepicker.esm.js";
-
 
 export default {
-
   components: {
     "content-app": Content,
+    Timeselector,
     Datepicker
   },
   created() {
     axios
-      .get("http://localhost:3000/usuarios")
-      .then(res => {
-        this.users = res.data;
-      })
-      .catch(e => {
-        alert("Não foi possivel carregar as informações dos usuarios.");
-        console.log(e);
-      });
-    axios
       .get("http://localhost:3000/ambientes")
       .then(res => {
-        // this.list = res.data;
+        this.ambientes = res.data;
       })
       .catch(e => {
         alert("Não foi possivel carregar as informações dos ambientes.");
         console.log(e);
-      });
+    });
+    axios
+      .get("http://localhost:3000/usuarios")
+      .then(res => {
+        this.usuarios = res.data;
+      })
+      .catch(e => {
+        alert("Não foi possivel carregar as informações dos usuarios.");
+        console.log(e);
+    });
+  },
+  mounted(){
+    const dataForm = document.getElementById('form-data')
+    dataForm.classList.add("form-control")
   },
   data() {
     return {
-      nome: "",
-      codigo: "",
       list: [],
-      users: [],
-      user:"",
-      ambientes: [],
+      usuarios: [],
+      usuario: "",
+      ambientes:[],
       ambiente:"",
-      indexEdicao: -1 //Index do usuario que esta sendo editado. -1 para novo usuario
+      data: null,
+      timeInicio: null,
+      timeFinal: null,
+      timeFinalError: "Defina um horário.",
+      indexEdicao: -1 //Index do ambiente que esta sendo editado. -1 para novo ambiente
     };
   },
   methods: {
-    salvarUsuario(){
-      let user = this.list[this.indexEdicao]
-      console.log(user)
+    overlap(x1, x2, y1, y2){
+      isOverlap = Math.max(x1,y1) <= Math.min(x2,y2)
+      console.log(isOverlap)
+      return isOverlap
+    },
+    ajustarHorario(data, horario){
+      horario.setDate(data.getDate())
+      horario.setYear(data.getYear())
+      horario.setMonth(data.getMonth())
+      console.log(horario)
+    },
+    gerarVetor(minimo, maximo, intervalo){
+      let array = []
+      for(i = minimo; i <= maximo; i+= intervalo){
+        array.push(i)
+      }
+      return array
+    },
+    salvar(){
+      let ambiente = this.list[this.indexEdicao]
       if(this.validarCampos()){
-        let index = this.indexEdicao
-        let user = this.list[index]
-        user.nome = this.nome
-        user.codigo = this.codigo
-        axios.put(`http://localhost:3000/usuarios/${user.id}`, user).catch(e => {
-          alert("Não foi possivel salvar as informações do usuario.");
+        ambiente.nome = this.nome
+        ambiente.lotacao = this.lotacao
+        ambiente.descricao = this.descricao
+        
+        axios.put(`http://localhost:3000/ambientes/${ambiente.id}`, ambiente).catch(e => {
+          alert("Não foi possivel salvar as informações do ambiente.");
           console.log(e);
         });
 
         this.limpaCampos()
       }
     },
-    criarUsuario() {
+    getUsuarioSelecionadoId(){
+      return this.$refs["form-usuario"].options[this.$refs["form-usuario"].options.selectedIndex].id;
+    },
+    getAmbienteSelecionadoId(){
+      return this.$refs["form-usuario"].options[this.$refs["form-usuario"].options.selectedIndex].id;
+    },
+    criar() {
+      let usuarioSelecionadoId = this.getUsuarioSelecionadoId()
+      let ambienteSelecionadoId = this.getAmbienteSelecionadoId()
       if (this.validarCampos()) {
-        let user = {
-          nome: this.nome,
-          codigo: this.codigo
+        let reserva = {
+          usuario: usuarioSelecionadoId,
+          ambiente: ambienteSelecionadoId,
+          timeInicio: this.timeInicio,
+          timeFinal: this.timeFinal,
         };
-        this.list.unshift(user); //adiciona no começo da lista
-        axios.post("http://localhost:3000/usuarios", user).catch(e => {
-          alert("Não foi possivel salvar as informações do usuario.");
+        //return;
+        axios.post("http://localhost:3000/reservas", reserva).then(res=>{
+          reserva.id = res.data.id
+          this.list.unshift(reserva); //adiciona no começo da lista
+          }).catch(e => {
+            alert("Não foi possivel salvar as informações da reserva.");
           console.log(e);
         });
         this.limpaCampos();
       }
     },
     validarCampos() {
-      let nomeError = 0;
-      let codigoError = 0;
-      if (this.nome == "") {
-        nomeError++;
+      if (this.usuario == "") {
+        this.$refs["form-usuario"].classList.add("is-invalid");
+        return false
       }
-      if (this.codigo == "") {
-        codigoError++;
+      this.$refs["form-usuario"].classList.remove("is-invalid");
+
+      if (this.ambiente == "") {
+        this.$refs["form-ambiente"].classList.add("is-invalid");
+        return false
       }
-        let bind_This = this;
-        this.list.forEach(function(usuario) {
-          if (usuario.codigo == bind_This.codigo)
-            if(bind_This.indexEdicao == -1){ //Esta fora do modo de edição
-              codigoError++;
-            }else{ //Modo de edição
-              if(bind_This.list[bind_This.indexEdicao].codigo != usuario.codigo)
-                codigoError++;
-            }
-        });
-      if (codigoError != 0) {
-        this.$refs["form-codigo"].classList.add("is-invalid");
-        return false;
+      this.$refs["form-ambiente"].classList.remove("is-invalid");
+      if (this.data == null) {
+        document.getElementById("form-data").classList.add("is-invalid");
+        return false
       }
-      if (nomeError != 0) {
-        this.$refs["form-nome"].classList.add("is-invalid");
-        return false;
+      document.getElementById("form-data").classList.remove("is-invalid");
+      
+
+      if (this.timeInicio == null) {
+        document.getElementById("form-timeInicio").parentNode.classList.add("is-invalid");
+        return false
       }
-      this.$refs["form-codigo"].classList.remove("is-invalid");
-      this.$refs["form-codigo"].classList.remove("is-invalid");
+      document.getElementById("form-timeInicio").parentNode.classList.remove("is-invalid");
+
+      if (this.timeFinal == null) {
+        document.getElementById("form-timeFinal").parentNode.classList.add("is-invalid");
+        this.timeFinalError = "Defina um horário."
+        return false
+      }
+      document.getElementById("form-timeFinal").parentNode.classList.remove("is-invalid");
+      
+      this.ajustarHorario(this.data, this.timeInicio)
+      this.ajustarHorario(this.data, this.timeFinal)
+
+      if(this.timeFinal.getTime() <= this.timeInicio.getTime()){
+        document.getElementById("form-timeFinal").parentNode.classList.add("is-invalid");
+        this.timeFinalError = "Duração de reserva inválida."
+        return false
+      }
+      document.getElementById("form-timeFinal").parentNode.classList.remove("is-invalid");
+
+      let timeInicio = this.timeInicio
+      let timeFinal = this.timeFinal
+      let usuarioSelecionadoId = this.getUsuarioSelecionadoId()
+      let ambienteSelecionadoId = this.getAmbienteSelecionadoId()
+      for (const evento in this.list) {
+        console.log(evento)
+        console.log(ambienteSelecionadoId)
+        if(evento.ambiente == ambienteSelecionadoId)
+          if(this.overlap(evento.timeInicio.getTime(), evento.timeFinal.getTime(), timeInicio.getTime(), timeFinal.getTime())){
+          document.getElementById("form-timeFinal").parentNode.classList.add("is-invalid");
+          this.timeFinalError = "Este ambiente já esta reservado nesse horário."
+          console.log("Em uso")
+          return false
+          }
+          document.getElementById("form-timeFinal").parentNode.classList.remove("is-invalid");
+      }
+      alert("deu ruim")
       return true;
     },
     limpaCampos() {
       this.nome = "";
-      this.codigo = "";
+      this.lotacao = "";
+      this.descricao = "";
       this.indexEdicao = -1;
     },
     obterDadosEvento(event){
@@ -184,24 +286,25 @@ export default {
       if(src.nodeName == "I")  src = src.parentNode //Garante que o src é o botão, e não o icone
       let row = src.parentNode.parentNode
       let index = row.attributes.index.value
-      let user = this.list[index]
-      user.index = index
-      return user
+      let ambiente = this.list[index]
+      ambiente.index = index
+      return ambiente
     },
     editar(event){
-      let usuario = this.obterDadosEvento(event)
-      this.indexEdicao = usuario.index
-      this.nome = usuario.nome
-      this.codigo = usuario.codigo
+      let ambiente = this.obterDadosEvento(event)
+      this.indexEdicao = ambiente.index
+      this.nome = ambiente.nome
+      this.descricao = ambiente.descricao
+      this.lotacao = ambiente.lotacao
     },
     remover(event){
-      let usuario = this.obterDadosEvento(event)
+      let ambiente = this.obterDadosEvento(event)
       let array = this.list
-      let index = usuario.index
+      let index = ambiente.index
       array.splice(index, 1);
       
-      axios.delete(`http://localhost:3000/usuarios/${usuario.id}`).catch(e => {
-          alert("Não foi possivel salvar as informações do usuario.");
+      axios.delete(`http://localhost:3000/ambientes/${ambiente.id}`).catch(e => {
+          alert("Não foi possivel salvar as informações do ambiente.");
           console.log(e);
       });
       
@@ -211,19 +314,7 @@ export default {
 </script>
 
 <style>
-
-#datepicker{
-    display: block;
-    height: calc(1.5em + .75rem + 2px);
-    padding: .375rem .75rem;
-    font-size: 1rem;
-    font-weight: 400;
-    line-height: 1.5;
-    color: #495057;
-    background-color:#fff;
-    background-clip: padding-box;
-    border: 1px solid#ced4da;
-    border-radius: .25rem;
-    transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+.vtimeselector__input {
+  border: 0;
 }
 </style>
